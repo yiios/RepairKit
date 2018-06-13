a0289e3c5bf0255b196adfdc320b95a3467e4376
Limo <limo@yiios.com>
2018-06-13 18:15:19 +0800

    :art: :  改进项目结构

--- RepairKit/WCRepairKitManage.h
+++ RepairKit/WCRepairKitManage.h
@@ -14,7 +14,7 @@
  数据库数据修复-非加密
 
  @param corruptedDBPath 已损坏的数据库路径
- @param pageSize 已损坏的数据库的page size
+ @param pageSize 损坏的数据库的页面大小。iOS上默认为4096。除非您可以调用PRAGMA page_size = NewPageSize来设置页面大小，否则页面大小不会改变。此外，您可以调用PRAGMA page_size来检查当前值，同时数据库没有损坏。
  @return 是否成功修复
  */
 - (BOOL)recoveryDBPath:(NSString *)corruptedDBPath DBPageSize:(int)pageSize;
@@ -31,7 +31,7 @@
  数据库数据修复-备份加密
 
  @param corruptedDBPath 已损坏的数据库路径
- @param pageSize 已损坏的数据库的page size
+ @param pageSize 损坏的数据库的页面大小。iOS上默认为4096。除非您可以调用PRAGMA page_size = NewPageSize来设置页面大小，否则页面大小不会改变。此外，您可以调用PRAGMA page_size来检查当前值，同时数据库没有损坏。
  @param backupCipherStr 元数据备份的密码，若备份未加密，则为 nil
  @return 是否成功修复
  */
